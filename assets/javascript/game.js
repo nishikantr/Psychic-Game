e28a03176bc684f71f9e1af6c91f1588cde83ff3
Nishikant Rane <nishikantrane@Nishikants-MacBook-Pro-2.local>
2017-08-10 19:29:15 -0500

    new changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,9 @@
-//var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-var computerChoices = ['a', 'b', 'c', 'd'];
 
 
 var wins = 0;
 var losses = 0;
-var guesses = 9;
 var guessesLeft = 9;
 var guessedLetters = [];
 var letterToGuess = null;
@@ -16,31 +14,34 @@ document.querySelector('#guessesLeft').innerHTML = "Guesses Left: " + guessesLef
 
 
 document.onkeyup = function(event) {
-	guessesLeft --;
+
 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-console.log(userGuess);
 
 	var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-
-console.log("computerGuess" + computerGuess);
 
 if (guessesLeft > 0) {
  		if (userGuess === computerGuess) {
  			wins++;
  			document.querySelector('#Wins').innerHTML = "Wins: " + wins;
- 			guessesLeft = guessesLeft - 1;
- 			console.log("guessesleft" + guessesLeft);
+ 			guessesLeft = 9 ;
+ 			document.querySelector('#guessesLeft').innerHTML = "Guesses Left: " + guessesLeft;
+
  		} else if (userGuess != computerGuess) {
- 			guessesLeft = guessesLeft - 1;
- 			console.log("loser guessesleft" + guesses);
+			 guessesLeft --;
+			 document.querySelector('#guessesLeft').innerHTML = "Guesses Left: " + guessesLeft;
+			 if (guessesLeft == 0 ) {
+			 	losses ++;
+	 			document.querySelector('#Losses').innerHTML = "Losses: " + losses;
+			    guessesLeft = 9 ;
+ 			    document.querySelector('#guessesLeft').innerHTML = "Guesses Left: " + guessesLeft;
+
+			 }
 
  		}
 
-} else if (guessesLeft = 0 ) {
-	alert("You lost, Play Again?")
-}
+} 
  
 }
 
